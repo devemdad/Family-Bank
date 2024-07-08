65a06c3dd83380dffaabc1f6fd80bd6d1ac573d2
devemdad <tipuwkc@gmail.com>
2024-07-08 23:01:22 +0600

    added deposit balance

---
+++ index.js
@@ -0,0 +1,57 @@
+//login button event handler
+
+const loginBtn = document.getElementById('login');
+loginBtn.addEventListener('click', function(){
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = 'none';
+    const transactionArea = document.getElementById('transaction-area');
+    transactionArea.style.display = 'block';
+})
+
+    //deposit button event handler
+
+    const depositButton = document.getElementById('addDeposit');
+    depositButton.addEventListener('click', function(){
+        const depositNumber = getInputNumber('depositAmount');
+
+        // const depositAmount = document.getElementById('depositAmount').value;
+        // const depositNumber = parseFloat(depositAmount);
+
+       /** 
+        const currentDeposit = document.getElementById('currentDeposit').innerText;
+        const currentDepositNumber = parseFloat(currentDeposit);
+        const totalDeposit = depositNumber + currentDepositNumber;
+        document.getElementById('currentDeposit').innerText = totalDeposit;   */
+
+        updateSpanText('currentDeposit', depositNumber);
+        updateSpanText('currentBalance', depositNumber);
+
+        document.getElementById('depositAmount').value = '';
+    })
+
+
+    //withdraw button event handler
+        const withdrawBtn = document.getElementById('addWithDraw');
+        withdrawBtn.addEventListener('click', function(){
+        const withdrawNumber = getInputNumber('withdrawAmount');
+        console.log(withdrawNumber);
+    })
+
+    function getInputNumber(id){
+        const amount = document.getElementById(id).value;
+        const amountNumber = parseFloat(amount);
+        return amountNumber;
+    }
+
+
+
+    function updateSpanText(id, depositNumber){
+        const current = document.getElementById(id).innerText;
+        const currentNumber = parseFloat(current);
+        const totalAmount = depositNumber + currentNumber;
+        // console.log(totalBalance);
+        document.getElementById(id).innerText = totalAmount;
+    }
+
+
+
